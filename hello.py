6ab3ae179e1b0177c8297a91cf88baa458adf3ce
Your Name <you@example.com>
2021-09-06 19:31:02 +0300

    commit from zdimon77@gmail.com ----

--- hello.py
+++ hello.py
@@ -1,6 +1,11 @@
 print('Hello World!!!!')
-age = input('What is your age?')
+#age = input('What is your age?')
+import random
 
+print(random.randint(1,9))
 
-
+#if age < 20:
+#    print('You are good!')
+#else:
+#    print('You are NOT good!')
 
