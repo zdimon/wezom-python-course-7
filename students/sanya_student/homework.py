4c4f34ed1d945584ae4862bafb4815228f887b9d
Your Name <you@example.com>
2021-09-09 19:47:31 +0300

    commit from zdimon77@gmail.com ----

--- students/sanya_student/homework.py
+++ students/sanya_student/homework.py
@@ -1,7 +1,7 @@
 import random
 
 number = int(input('Введите число: '))
-random_number = random.randint(1, 10)
+random_number = random.randint(1, 3)
 
 print(f'Ваше число - {number}')
 print(f'Случайное число - {random_number}')
