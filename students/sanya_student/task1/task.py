dc7777d24b970cfe437c9ef04ef6240a1c9171b9
yasandrkuba <alexandryakuba20@gmail.com>
2021-09-09 20:26:10 +0300

    asdf

---
+++ students/sanya_student/task1/task.py
@@ -0,0 +1,12 @@
+file1 = open('1.txt', 'r')
+file2 = open('2.txt', 'r')
+file3 = open('3.txt', 'r')
+
+for i in file1:
+    print(i)
+
+for i in file2:
+    print(i)
+
+for i in file3:
+    print(i)
