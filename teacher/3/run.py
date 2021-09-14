a0c37ec7d9325ada80124408dde1b3ba4b719ae4
Your Name <you@example.com>
2021-09-14 18:42:01 +0300

    commit from zdimon77@gmail.com ----

---
+++ teacher/3/run.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+print('Start')
+
+with open('people.txt', 'r') as file:
+    num = 1
+    for line in file.readlines():
+        # ['1.txt','Vasya']
+        line_list = line.split(';')
+        print(line_list[1])
+        # tmp_file = open(f'{num}.txt','w')
+        # tmp_file.write(line)
+        # tmp_file.close()
+        # num = num +1
