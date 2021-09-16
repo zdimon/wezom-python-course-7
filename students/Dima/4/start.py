823663f8a4c4114aefa0b1c131a1dba1f0067c5b
Your Name <you@example.com>
2021-09-16 18:15:05 +0300

    commit from zdimon77@gmail.com ----

---
+++ students/Dima/4/start.py
@@ -0,0 +1,3 @@
+from libs.lib import game
+
+game()
---
+++ teacher/4/start.py
@@ -0,0 +1,15 @@
+lst = [1,2,3,4]
+
+u = {
+    "name": "Sasha",
+    "age": 34,
+    "cards": [{"img": "/dama.png"}, {"img": "/dama.png"}]
+}
+
+faces = ["diamond", "heart", "spade", "clubs"]
+
+ranks = [2,3,4,5,6,7,8,9,10]
+
+# ["diamond2", "diamond3"]
+
+print(u["cards"][0]["img"])
