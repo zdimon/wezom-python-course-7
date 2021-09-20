cb5efb794225cb53c661f11932697fc723a022bd
Your Name <you@example.com>
2021-09-20 10:53:59 +0300

    commit from zdimon77@gmail.com ----

--- teacher/4/gamelib.py
+++ teacher/4/gamelib.py
@@ -1,11 +1,11 @@
 
 def make_deck():
     deck = []
-    faces = ["diamond", "heart", "spade", "clubs"]
+    faces = ["0", "1", "2", "3"]
     ranks = [2,3,4,5,6,7,8,9,10]
     for face in faces:
         for rank in ranks:
-            name = '%s-%s.png' % (face,rank)
+            name = '%s_%s.svg' % (face,rank)
             card = {
                 "name": name,
                 "rank": rank
--- teacher/4/start.py
+++ teacher/4/start.py
@@ -2,13 +2,22 @@ from gamelib import make_deck
 import telebot
 key = '1765842295:AAH_Vj8ti5WSjsU5mUFS7U3tOY2AmCinkAY'
 bot = telebot.TeleBot(key)
-
+import random
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
     #print(message)
     #print(message.from_user.username)
-    bot.send_message(message.chat.id, 'Привет %s, ты написал мне /start' % message.from_user.username)
+    deck = make_deck()
+    rc = random.randint(0,len(deck)-1)
+
+    bot.send_message(message.chat.id, deck[rc]["name"])
+
+    bot.send_photo(chat_id=message.chat.id, #photo=open(f'cards/{deck[rc]["name"]}', 'rb'))
+    photo=open('cards/1.jpeg','rb'))
+
+
+    #bot.send_message(message.chat.id, 'Привет %s, ты написал мне /start' % message.from_user.username)
 
 # deck = make_deck()
 # print(deck)
